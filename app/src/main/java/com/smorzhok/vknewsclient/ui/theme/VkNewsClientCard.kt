26361d1752ce7936fda1e54734b32aa6f86d24c0
vladryanka <vsmorzhok@mail.ru>
2025-05-29 18:42:06 +0300

    Новые иконки и рефакторинг

--- app/src/main/java/com/smorzhok/vknewsclient/ui/theme/VkNewsClientCard.kt
+++ app/src/main/java/com/smorzhok/vknewsclient/ui/theme/VkNewsClientCard.kt
@@ -10,8 +10,6 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.AccountCircle
-import androidx.compose.material.icons.rounded.Face
 import androidx.compose.material.icons.rounded.MoreVert
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
@@ -23,6 +21,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.smorzhok.vknewsclient.R
@@ -30,61 +29,101 @@ import com.smorzhok.vknewsclient.R
 @Composable
 fun VkNewsClientCard() {
     Card(Modifier.padding(4.dp)) {
-        Row(
-            modifier = Modifier
-                .padding(8.dp)
-                .fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Image(
-                painter = painterResource(R.drawable.ic_launcher_background),
-                contentDescription = "",
-                modifier = Modifier
-                    .clip(CircleShape)
-                    .size(50.dp)
-            )
-            Spacer(modifier = Modifier.width(8.dp))
-            Column(modifier = Modifier.weight(1f)) {
-                Text(text = "Уволено", color = MaterialTheme.colorScheme.onPrimary)
-                Text(text = "14:00", color = MaterialTheme.colorScheme.onSecondary)
-            }
-            Icon(
-                imageVector = Icons.Rounded.MoreVert,
-                contentDescription = "",
-                tint = MaterialTheme.colorScheme.onSecondary
-            )
-        }
+        PostHeader()
         Text(
             modifier = Modifier
                 .padding(8.dp)
                 .fillMaxWidth(),
             color = MaterialTheme.colorScheme.onPrimary,
-            text = "Кабаныч, когда узнал, что если сотрудникам не платить они начинают умирать от голода"
+            text = stringResource(R.string.template_text)
         )
         Image(
             painter = painterResource(R.drawable.ic_launcher_background),
-            contentDescription = "",
+            contentDescription = null,
             modifier = Modifier
                 .padding(8.dp)
                 .fillMaxWidth(),
             contentScale = ContentScale.FillWidth
         )
-        Row(modifier = Modifier.padding(8.dp)) {
-            Text(text = "206", color = MaterialTheme.colorScheme.onSecondary)
-            Icon(Icons.Rounded.AccountCircle, contentDescription = "")
-            Spacer(
-                modifier = Modifier.weight(1f)
-            )
-            Text(text = "206", color = MaterialTheme.colorScheme.onSecondary)
-            Icon(imageVector = Icons.Rounded.MoreVert, contentDescription = "")
-            Text(text = "11", color = MaterialTheme.colorScheme.onSecondary)
-            Icon(imageVector = Icons.Rounded.Face, contentDescription = "")
-            Text(text = "491", color = MaterialTheme.colorScheme.onSecondary)
-            Icon(imageVector = Icons.Rounded.MoreVert, contentDescription = "")
-        }
+        Statistics()
     }
 }
 
+@Composable
+fun Statistics() {
+    Row(
+        modifier = Modifier.padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            painterResource(R.drawable.ic_eye), contentDescription = null,
+            tint = MaterialTheme.colorScheme.onSecondary,
+            modifier = Modifier.padding(4.dp)
+        )
+        Text(
+            text = "206", color = MaterialTheme.colorScheme.onSecondary,
+            modifier = Modifier.padding(4.dp)
+        )
+        Spacer(
+            modifier = Modifier.weight(1f)
+        )
+        Text(
+            text = "206", color = MaterialTheme.colorScheme.onSecondary,
+            modifier = Modifier.padding(4.dp)
+        )
+        Icon(
+            painterResource(R.drawable.ic_arrow_outward), contentDescription = null,
+            tint = MaterialTheme.colorScheme.onSecondary,
+            modifier = Modifier.padding(4.dp)
+        )
+        Text(
+            text = "11", color = MaterialTheme.colorScheme.onSecondary,
+            modifier = Modifier.padding(4.dp)
+        )
+        Icon(
+            painterResource(R.drawable.comment), contentDescription = null,
+            tint = MaterialTheme.colorScheme.onSecondary,
+            modifier = Modifier.padding(4.dp)
+        )
+        Text(
+            text = "491", color = MaterialTheme.colorScheme.onSecondary,
+            modifier = Modifier.padding(4.dp)
+        )
+        Icon(
+            painter = painterResource(R.drawable.favorite), contentDescription = null,
+            tint = MaterialTheme.colorScheme.onSecondary,
+            modifier = Modifier.padding(4.dp)
+        )
+    }
+}
+
+@Composable
+fun PostHeader() {
+    Row(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painter = painterResource(R.drawable.ic_launcher_background),
+            contentDescription = null,
+            modifier = Modifier
+                .clip(CircleShape)
+                .size(50.dp)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Column(modifier = Modifier.weight(1f)) {
+            Text(text = "Уволено", color = MaterialTheme.colorScheme.onPrimary)
+            Text(text = "14:00", color = MaterialTheme.colorScheme.onSecondary)
+        }
+        Icon(
+            imageVector = Icons.Rounded.MoreVert,
+            contentDescription = null,
+            tint = MaterialTheme.colorScheme.onSecondary
+        )
+    }
+}
 
 @Preview
 @Composable
