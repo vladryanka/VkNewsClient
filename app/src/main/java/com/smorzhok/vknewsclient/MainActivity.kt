becf48e904b477053533552788aa207791012c7a
vladryanka <vsmorzhok@mail.ru>
2025-05-29 19:54:04 +0300

    Scaffold и BottomNavigation

--- app/src/main/java/com/smorzhok/vknewsclient/MainActivity.kt
+++ app/src/main/java/com/smorzhok/vknewsclient/MainActivity.kt
@@ -4,43 +4,60 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.smorzhok.vknewsclient.ui.theme.VkNewsClientCard
 import com.smorzhok.vknewsclient.ui.theme.VkNewsClientTheme
 
 class MainActivity : ComponentActivity() {
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             VkNewsClientTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    VkNewsClientCard()
-                }
+
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Test() {
+    Scaffold(topBar = {
+        TopAppBar(
+            title = { Text(text = "Simple TopAppBar") },
+            navigationIcon = { Icon(Icons.Filled.Menu, contentDescription = null) },
+            actions = {Icon(Icons.Default.Close, contentDescription = null)}
+        )
+    },
+        bottomBar = {
+            BottomAppBar(contentColor = Color.Black) {
+                Icon(Icons.Filled.Favorite, contentDescription = null)
+            }
+        }) {
+        Text(text = "jjj", Modifier.padding(it))
+    }
+
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     VkNewsClientTheme {
-        Greeting("Android")
+        Test()
     }
 }
