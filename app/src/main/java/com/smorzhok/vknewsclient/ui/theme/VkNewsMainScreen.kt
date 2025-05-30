e5ac644dc2244790284a2baeef84dacb176ae255
vladryanka <vsmorzhok@mail.ru>
2025-05-31 01:49:23 +0300

    FAB и SnackBar

--- app/src/main/java/com/smorzhok/vknewsclient/ui/theme/VkNewsMainScreen.kt
+++ app/src/main/java/com/smorzhok/vknewsclient/ui/theme/VkNewsMainScreen.kt
@@ -1,30 +1,40 @@
 package com.smorzhok.vknewsclient.ui.theme
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import kotlinx.coroutines.launch
 
 @SuppressLint("UnrememberedMutableState")
 @Preview
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen() {
+
+    val snackBarHostState = SnackbarHostState()
+    val fabVisibilityState = remember { mutableStateOf(true) }
+    val scope = rememberCoroutineScope()
     Scaffold(
         topBar = {
             TopAppBar(
@@ -32,6 +42,28 @@ fun MainScreen() {
                 navigationIcon = { Icon(Icons.Filled.Menu, contentDescription = null) },
                 actions = { Icon(Icons.Default.Close, contentDescription = null) }
             )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    scope.launch {
+                        val action = snackBarHostState.showSnackbar(
+                            "message",
+                            actionLabel = "close"
+                        )
+                        if (action == SnackbarResult.ActionPerformed) {
+                            fabVisibilityState.value = false
+                        }
+                    }
+                },
+                containerColor = MaterialTheme.colorScheme.primary,
+                shape = CircleShape
+            ) {
+                Icon(Icons.Default.Favorite, contentDescription = null)
+            }
+        },
+        snackbarHost = {
+            SnackbarHost(hostState = snackBarHostState)
         },
         bottomBar = {
             BottomAppBar(containerColor = MaterialTheme.colorScheme.surface) {
@@ -62,7 +94,7 @@ fun MainScreen() {
                 }
             }
         }) {
-        Text(text = "jjj", Modifier.padding(it))
+        it
     }
 
 }
