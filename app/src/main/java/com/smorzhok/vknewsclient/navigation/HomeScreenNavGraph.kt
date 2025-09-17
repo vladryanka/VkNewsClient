b574c791f35bb985e489c858024d1ff3073cccee
vladryanka <vsmorzhok@mail.ru>
2025-09-17 14:59:24 +0300

    Revert "передача аргументов через навигацию"
    This reverts commit 28b34a40c63d2a3c2c1c3bcf8e560219ee81854d.

--- app/src/main/java/com/smorzhok/vknewsclient/navigation/HomeScreenNavGraph.kt
+++ app/src/main/java/com/smorzhok/vknewsclient/navigation/HomeScreenNavGraph.kt
@@ -2,9 +2,7 @@ package com.smorzhok.vknewsclient.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavGraphBuilder
-import androidx.navigation.NavType
 import androidx.navigation.compose.composable
-import androidx.navigation.navArgument
 import androidx.navigation.navigation
 import com.smorzhok.vknewsclient.domain.FeedPost
 
@@ -19,18 +17,9 @@ fun NavGraphBuilder.homeScreenNavGraph(
         composable(Screen.NewsFeed.route) {
             newsFeedScreenContent()
         }
-        composable(Screen.Comments.route,
-            arguments = listOf (
-                navArgument(Screen.KEY_FEED_POST_ID){
-                    type = NavType.IntType
-                },
-                navArgument(Screen.KEY_CONTENT_TEXT){
-                    type = NavType.StringType
-                }
-            )) {
+        composable(Screen.Comments.route) {
             val feedPostId = it.arguments?.getInt(Screen.KEY_FEED_POST_ID) ?: 0
-            val contentText = it.arguments?.getString(Screen.KEY_CONTENT_TEXT) ?: ""
-            commentsScreenContent(FeedPost(id = feedPostId, postText = contentText))
+            commentsScreenContent(FeedPost(id = feedPostId))
         }
     }
 
